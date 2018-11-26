154640b3ab3ba31e9800eed137be8d106589297a
Justin Chiu <justinchiu@g.harvard.edu>
2018-11-26 14:41:33 -0500

    stuff

--- vi/models/base.py
+++ vi/models/base.py
@@ -48,6 +48,7 @@ class Lm(nn.Module):
             for i, batch in enumerate(t):
                 if learn:
                     optimizer.zero_grad()
+                import pdb; pdb.set_trace()
                 text, lens = batch.text
                 x = text[:-1]
                 y = text[1:]
@@ -109,5 +110,60 @@ class Lm(nn.Module):
 class Ie(nn.Module):
     PAD = "<pad>"
 
-    def _loop_ie(self):
-        pass
+    def _loop_ie(self, iter, optimizer=None, clip=0, learn=False, re=None):
+        context = torch.enable_grad if learn else torch.no_grad
+
+        cum_loss = 0
+        cum_ntokens = 0
+        cum_rx = 0
+        cum_kl = 0
+        batch_loss = 0
+        batch_ntokens = 0
+        states = None
+        with context():
+            t = tqdm(iter) if learn else iter
+            for i, batch in enumerate(t):
+                if learn:
+                    optimizer.zero_grad()
+                text, lens = batch.text
+                x = text[:-1]
+                y = text[1:]
+                lens = lens - 1
+                # should i include <eos> in ppl?
+                nwords = y.ne(1).sum()
+                # assert nwords == lens.sum()
+                T, N = y.shape
+                #if states is None:
+                states = self.init_state(N)
+                logits, _ = self(x, states, lens)
+                #logits, states = self(x, states, lens)
+                """
+                if learn:
+                    states = (
+                        [tuple(x.detach() for x in tup) for tup in states[0]],
+                        tuple(x.detach() for x in states[1]),
+                        states[2].detach(),
+                    )
+                """
+                logprobs = F.log_softmax(logits, dim=-1)
+                logp = logprobs.view(T*N, -1).gather(-1, y.view(T*N, 1))
+                kl = 0
+                nll = -logp[y.view(-1, 1) != 1].sum()
+                nelbo = nll + kl
+                if learn:
+                    nelbo.div(nwords.item()).backward()
+                    if clip > 0:
+                        gnorm = clip_(self.parameters(), clip)
+                        #for param in self.rnn_parameters():
+                            #gnorm = clip_(param, clip)
+                    optimizer.step()
+                cum_loss += nelbo.item()
+                cum_ntokens += nwords.item()
+                batch_loss += nelbo.item()
+                batch_ntokens += nwords.item()
+                if re is not None and i % re == -1 % re:
+                    t.set_postfix(loss = batch_loss / batch_ntokens, gnorm = gnorm)
+                    batch_loss = 0
+                    batch_ntokens = 0
+        return cum_loss, cum_ntokens
+
